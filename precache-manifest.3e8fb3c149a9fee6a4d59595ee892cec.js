c7493903446e4fe879b48e0e99e67358604770d6
Valerie Beltrame <beltrame.valerie@gmail.com>
2018-12-05 18:09:53 +0100

    Updates

---
+++ precache-manifest.3e8fb3c149a9fee6a4d59595ee892cec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "142067362599cd8187aa71f4eef67dc2",
+    "url": "/julebankoo/static/media/snow.14206736.svg"
+  },
+  {
+    "revision": "a1326e45abf8d46c1e2d6e9d4f203b40",
+    "url": "/julebankoo/static/media/sleigh.a1326e45.svg"
+  },
+  {
+    "revision": "ddfb8f4552ab99c7310146656949ce8c",
+    "url": "/julebankoo/static/media/present.ddfb8f45.svg"
+  },
+  {
+    "revision": "4d50fe69fa0c0d8d55ba",
+    "url": "/julebankoo/static/js/runtime~main.4d50fe69.js"
+  },
+  {
+    "revision": "07aff5c89196c2f9d345",
+    "url": "/julebankoo/static/js/main.07aff5c8.chunk.js"
+  },
+  {
+    "revision": "5e440350d73918ecad1d",
+    "url": "/julebankoo/static/js/1.5e440350.chunk.js"
+  },
+  {
+    "revision": "07aff5c89196c2f9d345",
+    "url": "/julebankoo/static/css/main.7515181a.chunk.css"
+  },
+  {
+    "revision": "58243ee46e1c6ec1f7c8bc55331e614d",
+    "url": "/julebankoo/index.html"
+  }
+];
